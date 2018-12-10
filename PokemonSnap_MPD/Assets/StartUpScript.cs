9c4c088cc6cee979e02b91edc308e7a53af0753c
Atlas <AtlasTheTitan77@gmail.com>
2018-12-10 18:10:36 +0100

    Better Setup
    VR has been added

---
+++ PokemonSnap_MPD/Assets/StartUpScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUpScript : MonoBehaviour {
+
+	public GameObject vrPrefab;
+	public GameObject phonePrefab;
+	public GameObject editorPrefab;
+	public GameObject TestObject;
+	void Start () {
+		#if UNITY_EDITOR
+		Instantiate(editorPrefab, transform.position, transform.rotation);
+      	Debug.Log("Unity Editor");
+		TestObject.GetComponent<Renderer>().material.color = Color.blue;
+		#elif UNITY_STANDALONE_WIN
+		Instantiate(vrPrefab, transform.position, transform.rotation);
+		Debug.Log("Stand Alone Windows");
+		TestObject.GetComponent<Renderer>().material.color = Color.red;
+		#endif
+		
+		#if UNITY_ANDROID
+		Instantiate(phonePrefab, transform.position, transform.rotation);
+		Debug.Log("Iphone");
+		TestObject.GetComponent<Renderer>().material.color = Color.green;
+		#endif
+
+		
+	}
+	
+	void Update () {
+		
+	}
+}
