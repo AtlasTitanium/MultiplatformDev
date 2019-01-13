c480a2acb8f1d7c860e0838d58091f2c8cf72ac9
Atlas <AtlasTheTitan77@gmail.com>
2019-01-13 23:47:29 +0100

    Something, yet nothing

---
+++ PokemonSnap_MPD/Assets/PhoneUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneUI : MonoBehaviour {
+	GUIStyle button;
+	public Texture2D shoot;
+	public GameObject oof;
+	public Mesh meshia;
+	public GameObject newCamera;
+
+	void Start(){
+		button = new GUIStyle("button");
+	}
+
+	void Update(){
+		oof = GameObject.Find("Floor");
+		newCamera = GameObject.FindGameObjectWithTag("NewCamera");
+	}
+
+	void OnGUI(){
+		button.normal.background = shoot;
+		if(GUI.Button(new Rect(Screen.width - Screen.width/4, Screen.height - Screen.height/4, Screen.width/4, Screen.height/4), "", button)){
+			Shoot();
+		}
+	}
+
+	void Shoot(){
+		GameObject gm = new GameObject();
+		gm.transform.position = newCamera.transform.position;
+		gm.AddComponent<MeshFilter>().mesh = meshia;
+		gm.AddComponent<MeshRenderer>();
+		gm.AddComponent<SphereCollider>();
+		Rigidbody rb = gm.AddComponent<Rigidbody>();
+		rb.AddForce(Camera.current.transform.forward * 1000);
+
+		oof.GetComponent<Renderer>().material.color = Color.magenta;
+	}
+}
--- PokemonSnap_MPD/Assets/StartUpScript.cs
+++ PokemonSnap_MPD/Assets/StartUpScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class StartUpScript : MonoBehaviour {
 
@@ -8,27 +9,23 @@ public class StartUpScript : MonoBehaviour {
 	public GameObject phonePrefab;
 	public GameObject editorPrefab;
 	public GameObject TestObject;
+	public GameObject mainCamera;
 	void Start () {
 		#if UNITY_EDITOR
 		Instantiate(editorPrefab, transform.position, transform.rotation);
+		mainCamera.SetActive(false);
       	Debug.Log("Unity Editor");
 		TestObject.GetComponent<Renderer>().material.color = Color.blue;
 		#elif UNITY_STANDALONE_WIN
 		Instantiate(vrPrefab, transform.position, transform.rotation);
+		mainCamera.SetActive(false);
 		Debug.Log("Stand Alone Windows");
 		TestObject.GetComponent<Renderer>().material.color = Color.red;
-		#endif
-		
-		#if UNITY_ANDROID
+		#elif UNITY_ANDROID
 		Instantiate(phonePrefab, transform.position, transform.rotation);
+		XRSettings.enabled = false;
 		Debug.Log("Iphone");
 		TestObject.GetComponent<Renderer>().material.color = Color.green;
 		#endif
-
-		
-	}
-	
-	void Update () {
-		
 	}
 }
