35c15c9e1e8d1381acc7c722eea59e214fc84b50
Atlas <AtlasTheTitan77@gmail.com>
2019-01-15 23:20:32 +0100

    addedDebug

---
+++ PokemonSnap_MPD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+	private GameObject WaitArea;
+	private GameObject StartLocation;
+	private GameObject EndLocation;
+	private GameObject Vr;
+	private GameObject Mobile;
+	void Start(){
+		#if UNITY_EDITOR
+		if(isServer){
+			RpcChangePlayer(true,false);
+		}
+		if(isClient){
+			CmdChangePlayer(true,false);
+		} else {
+			Debug.Log("not online");
+		}
+
+		#elif UNITY_STANDALONE_WIN
+		if(isServer){
+			RpcChangePlayer(true,false);
+		}
+		if(isClient){
+			CmdChangePlayer(true,false);
+		} else {
+			Debug.Log("not online");
+		}
+
+		#elif UNITY_ANDROID
+		if(isServer){
+			CmdChangePlayer(false,true);
+		}
+		if(isClient){
+			CmdChangePlayer(false,true);
+		} else {
+			Debug.Log("not online");
+		}
+		#endif
+	}
+
+	[Command]
+	public void CmdChangePlayer(bool vr, bool mobile){
+		RpcChangePlayer(vr,mobile);
+	}
+
+	[ClientRpc]
+	public void RpcChangePlayer(bool vr, bool mobile){
+		if(vr){
+			Mobile = transform.GetChild(1).gameObject;
+			Debug.Log(Mobile);
+			Mobile.SetActive(false);
+
+			Debug.Log("StartVr");
+			this.GetComponent<NetworkStarter>().enabled = false;
+
+			WaitArea = GameObject.FindGameObjectWithTag("VrWait");
+			//StartLocation = GameObject.FindGameObjectWithTag("VrStart");
+			//EndLocation = GameObject.FindGameObjectWithTag("VrEnd");
+
+			if(localPlayerAuthority && WaitArea != null){
+				transform.position = WaitArea.transform.position;
+			}
+		}
+		if(mobile){
+			Vr = transform.GetChild(0).gameObject;
+			Debug.Log(Vr);
+			Vr.SetActive(false);
+
+			Debug.Log("StartMobile");
+			this.GetComponent<NetworkStarter>().enabled = false;
+
+			WaitArea = GameObject.FindGameObjectWithTag("MobileWait");
+			//StartLocation = GameObject.FindGameObjectWithTag("MobileStart");
+			//EndLocation = GameObject.FindGameObjectWithTag("MobileEnd");
+
+			if(localPlayerAuthority && WaitArea != null){
+				transform.position = WaitArea.transform.position;
+			}
+		}
+	}
+}
---
+++ PokemonSnap_MPD/Assets/_Debug_/DebugPlayerCreate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class DebugPlayerCreate : NetworkBehaviour {
+	private GameObject floor;
+	public GameObject Camera;
+	private GameObject CameraLocation;
+
+	void Start () {
+		floor = GameObject.FindGameObjectWithTag("Debug");
+		#if UNITY_ANDROID
+		Debug.Log("IsMobile");
+		CameraLocation = GameObject.FindGameObjectWithTag("MobileStart");
+		floor.GetComponent<Renderer>().material.color = Color.blue;  
+		#elif UNITY_STANDALONE_WIN
+		Debug.Log("IsStandalone");
+		CameraLocation = GameObject.FindGameObjectWithTag("VrStart");
+		floor.GetComponent<Renderer>().material.color = Color.red;  
+		#endif
+
+		if(CameraLocation != null){
+			Camera.transform.position = CameraLocation.transform.position;
+			Camera.transform.rotation = CameraLocation.transform.rotation;
+		} else {
+			Debug.Log("nothing found");
+		}
+	}
+
+	void OnApplicationQuit(){
+		if(isServer){
+			NetworkManager.singleton.StopHost();
+			NetworkManager.singleton.StopServer();
+		}
+		if(isClient){
+			NetworkManager.singleton.StopClient();
+		}
+	}
+}
